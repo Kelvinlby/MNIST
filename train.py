7a77b4503ca92b43e7fbf94b6807ced908a8399d
Kelvin <76610777+Kelvinlby@users.noreply.github.com>
2024-08-08 10:47:26 +0800

    update

--- train.py
+++ train.py
@@ -16,8 +16,8 @@ def main():
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data('/mnt/Data/Dataset/mnist.npz')
 
     # Scale images to the [0, 1] range
-    x_train = x_train.astype("float16") / 255
-    x_test = x_test.astype("float16") / 255
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
     # Make sure images have shape (28, 28, 1)
     x_train = np.expand_dims(x_train, -1)
     x_test = np.expand_dims(x_test, -1)
